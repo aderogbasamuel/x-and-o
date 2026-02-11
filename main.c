35061d1159f52afca261987b374efd2035e142f5
aderogbasamuel <118081549+aderogbasamuel@users.noreply.github.com>
2026-02-11 18:58:16 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+void displayBoard(char board[]);
+int checkWin(char board[]);
+int main(){
+    char board [9]={'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char turn='X'; 
+    int position;
+    while(1){
+        displayBoard(board);
+        printf("\n%c turn, enter number position to play: ", turn);
+        scanf("%d", &position);
+        if(position > 0|| position < 10){
+            board[position-1]=turn;
+            printf("X played");
+            displayBoard(board);
+            if(checkWin(board)==1){
+                break;
+            }
+            turn=(turn=='x')? 'O': 'X';
+        }
+        else{
+            printf("Invalid input, try another one\n");
+        }
+    }
+    printf("\n");
+    return 0;
+}
+
+void displayBoard(char board[]){
+    printf("\n");
+
+    for (int i = 0; i < 9; i++) {
+        printf(" %c ", board[i]);
+
+        if ((i + 1) % 3 != 0) {
+            printf("|");
+        } else if (i != 8) {
+            printf("\n---+---+---\n");
+        }
+    }
+    printf("\n");
+}
+
+int checkWin(char board[]){
+    int i;
+
+    for(i=0; i<9; i+=3){
+        if(board[i]==board[i+1]&&board[i+1]==board[i+2]&&board[i]==('X'|'O')){
+            printf("%c won",board[i]);
+            return 1;
+        }
+    }
+    return 0;
+}
