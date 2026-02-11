2ac323ef9f60a5d5c836bfd50c1da0cded77685e
aderogbasamuel <118081549+aderogbasamuel@users.noreply.github.com>
2026-02-11 19:30:09 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void displayBoard(char board[]);
 int checkWin(char board[]);
+
+//omo guy, omo again
 int main(){
     char board [9]={'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     char turn='X'; 
@@ -15,8 +17,9 @@ int main(){
             displayBoard(board);
             if(checkWin(board)==1){
                 break;
+                
             }
-            turn=(turn=='x')? 'O': 'X';
+            turn=(turn=='X')? 'O': 'X';
         }
         else{
             printf("Invalid input, try another one\n");
@@ -45,10 +48,24 @@ int checkWin(char board[]){
     int i;
 
     for(i=0; i<9; i+=3){
-        if(board[i]==board[i+1]&&board[i+1]==board[i+2]&&board[i]==('X'|'O')){
+        if(board[i]==board[i+1]&&board[i+1]==board[i+2]&&(board[i]=='X'||board[i]=='O')){
             printf("%c won",board[i]);
             return 1;
         }
+    }
+    for(i=0; i<9; i+=2){
+        if(board[i]==board[i+3]&&board[i+3]==board[i+6]&&(board[i]=='X'||board[i]=='O')){
+            printf("%c won",board[i]);
+            return 1;
+        }
+    }
+    if(board[0]==board[4]&&board[4]==board[8]&&(board[0]=='X'||board[0]=='O')){
+        printf("%c won",board[i]);
+            return 1;
+    }
+    if(board[2]==board[4]&&board[4]==board[6]&&(board[2]=='X'||board[2]=='O')){
+        printf("%c won",board[i]);
+            return 1;
     }
     return 0;
 }
